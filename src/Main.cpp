81cc9d5ab45ce6c7077b16f71899c28ae847d624
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-05-06 13:22:25 +0800

    Adding dependencies

---
+++ src/Main.cpp
@@ -0,0 +1,11 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+int main() {
+
+}
