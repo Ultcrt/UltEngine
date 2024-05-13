263361372e433fcec9d6ac8392bb97d3a6086ff0
Ultcrt <zhang_zhe_yuan@outlook.com>
2024-05-13 18:32:36 +0800

    Implementing post processing

---
+++ include/PostProcessing.h
@@ -0,0 +1,14 @@
+//
+// Created by ultcrt on 24-5-13.
+//
+
+#ifndef ULTENGINE_POSTPROCESSING_H
+#define ULTENGINE_POSTPROCESSING_H
+
+namespace UltEngine {
+    class PostProcessing {
+    private:
+    };
+} // UltEngine
+
+#endif //ULTENGINE_POSTPROCESSING_H
---
+++ src/PostProcessing.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ultcrt on 24-5-13.
+//
+
+#include "PostProcessing.h"
+
+namespace UltEngine {
+} // UltEngine
